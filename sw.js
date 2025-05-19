4bc476bd1866fde83e52d0465ad20f443829d940
jhgonzalezuruguay <102374190+jhgonzalezuruguay@users.noreply.github.com>
2025-05-19 16:50:29 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Puedes implementar caché aquí si lo deseas para trabajar offline
+});
